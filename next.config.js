128c326451f07407292a327a253d0cb112face51
minholeelog <91aiden.lee@gmail.com>
2022-01-04 17:27:04 +0900

    Dynamic routes

--- next.config.js
+++ next.config.js
@@ -6,7 +6,11 @@ module.exports = {
     return [
       {
         source: '/api/movies',
-        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: '/api/movies/:id',
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
       },
     ];
   },
